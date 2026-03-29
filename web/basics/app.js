4436955c5e0e516cbbc97e75a2fc38af84eef18c
ludmanyboglarka <lboglarka25@gmail.com>
2026-03-29 22:29:27 +0200

    the absolute basics

--- web/basics/app.js
+++ web/basics/app.js
@@ -1,1 +1,36 @@
-console.log('Hello World!');
+let experiment = 'Stroop';
+console.log(typeof experiment); // string
+console.log(experiment.charAt(0)); // 'S'
+
+let welcomeMessage =
+    `Welcome to our ` + experiment + ` experiment!
+    Please read the instructions carefully.
+    Welcome to our ` + experiment + ` experiment!
+    Please read the instructions carefully.
+    Welcome to our ` + experiment + ` experiment!
+    Please read the instructions carefully.
+    `;
+
+console.log(welcomeMessage);
+
+let trialCountMax = 20;
+console.log(typeof trialCountMax); // number
+
+
+// TODO: Randomize colors:
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+
+let halfWayCount = trialCountMax / 2;
+
+console.log(halfWayCount); // Expected: 20
+
+let correct = true;
+console.log(typeof correct);
+
+console.log(10 <= 15); // true
+
